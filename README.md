5b753fbb8fac9a37b64b2b9e11093a7a149ea873
lidiamayor <lidiamayor2000@gmail.com>
2024-09-12 23:38:13 +0200

    all-files

--- README.md
+++ README.md
@@ -1,29 +1,60 @@
-# Marketing Strategy Analysis Project for a Wine Production Company
 
-## Description
-This project is the result of the work of **Lidia Mayor** and **Ana Nofuentes**, students in a data analysis bootcamp, where we represent a consulting firm specializing in marketing strategies. Our goal is to help a wine production company identify key customers to maximize the profitability of their sales.
+---
 
-### Project Objectives:
-1. **Identify key customers**: Analyze the data to determine which customer profiles are the most profitable.
-2. **Optimize marketing strategies**: Provide recommendations on where to focus marketing efforts to improve sales.
+# 📊 Data Analysis for Wine Consumption
 
-## Project Contents
+Hello, data enthusiasts! 🌟 We are [Ana Nofuentes](https://www.linkedin.com/in/ana-nofuentes-solano-654026a3/) and [Lidia Mayor](https://www.linkedin.com/in/lidia-mayor-sanjuan-3b350930b/), students from a data analysis bootcamp. This notebook is a comprehensive guide to our project focused on analyzing wine consumption data. Our aim is to provide insightful customer segmentation to help a wine producer boost their sales.
 
-### 1. **Data Loading**
-   We load the data provided by the company to start the analysis.
+## 📈 Notebook Overview
 
-### 2. **Library Initialization**
-   We initialize the necessary libraries to manipulate and analyze the data.
+### Data Loading:
+We start by initializing the necessary libraries and importing the wine consumption dataset.
 
-### 3. **Feature Engineering**
-   We transform raw data into more informative features to perform a more precise analysis.
+### Feature Engineering:
+Transforming raw data into informative features to enhance the precision of our analysis.
 
-### 4. **Data Cleaning**
-   We remove null values, duplicates, and correct inconsistent data to prepare the data for analysis.
+### Data Cleaning:
+We meticulously clean the data by removing null values, duplicates, and correcting inconsistencies to ensure accurate analysis.
 
-### 5. **Exploratory Data Analysis (EDA)**
-   We analyze the distribution of customer age to identify relevant behavioral patterns.
+### Exploratory Data Analysis (EDA):
+We delve into the distribution of wine consumption by age and gender to uncover patterns and trends.
 
-## Authors
-- **Lidia Mayor** - Bootcamp Student
-- **Ana Nofuentes** - Bootcamp Student
+## 📸 Key Visual Insights
+
+[Wine Consumption by age](https://drive.google.com/file/d/1aor3SO261VdDts6svA4yXpBKqCCsFZow/view?usp=sharing) 
+*Visualizations showing how wine consumption varies across different age groups.*
+[Purchase behavior analysis](https://drive.google.com/file/d/1ZqHfDMRRdVH6n7wBsmyPeP-r6wXR_yAC/view?usp=sharing) 
+*Graphs illustrating purchasing based on income.*
+[Interest in ads by website category](https://drive.google.com/file/d/1Oj4Gbik10aGrVJaJYsnmv0Km6WevTDlZ/view?usp=sharing)
+*Insights into how the website category influences interest in ads among people with diferent incomes*
+
+
+## 📊 Storytelling Presentation
+
+We have compiled a detailed presentation summarizing the key findings and conclusions from our analysis. You can view the full presentation of the storytelling and conclusions by following the link below:
+
+[📑 Storytelling Presentation](https://www.canva.com/design/DAGQkrpshQM/nB72tfU4HS692wTwG_xqcg/view?utm_content=DAGQkrpshQM&utm_campaign=designshare&utm_medium=link&utm_source=editor)  
+*This presentation highlights the main insights, conclusions, and actionable recommendations based on the wine consumption data.*
+
+
+## 🌐 Streamlit App
+
+We have developed a Streamlit app based on our data analysis, allowing users to interactively explore the data and visualize key insights. You can find the app's code in the following GitHub repository:
+
+[🖥️ Streamlit Repository](https://github.com/lidiamayor/marketing-study-project-streamlit)  
+*Explore the app and interact with the data visualizations in real-time.*
+
+
+## 🤖 About Us
+
+We are Ana and Lidia, passionate students committed to turning data into actionable insights. Through our project, we aim to empower businesses with data-driven strategies to optimize their marketing efforts.
+
+Special Note: For businesses looking for in-depth data analysis and customer segmentation, feel free to reach out to us. We're here to help you harness the power of your data.
+
+## 📩 Get in Touch
+
+Ready to dive into data with us? Contact us to explore how we can support your business goals!
+
+📔 Explore the Notebook: For a detailed dive, explore the Notebook on  [GitHub](https://github.com/lidiamayor/marketing-study-project/blob/main/main.ipynb).
+
+---
--- functions_click.py
+++ functions_click.py
@@ -4,77 +4,151 @@ import seaborn as sns
 import numpy as np
 
 
-def clean_click(df):
+def clean_df_click(df):
+    """
+    This function takes a pandas DataFrame (`df`) and cleans/preprocesses it.
+    It removes the columns: 'Unnamed: 0', 'Location', 'Device', 'Time_Spent_on_Site', 'Number_of_Pages_Viewed',
+    and creates two new columns: 'Income_Range' and 'Age_Range', that are categorical bins of the 'Income' and 'Age' columns, respectively.
+    """
+
+    # drop columns that are not useful for the analysis
     df = df.drop(columns=['Unnamed: 0', 'Location', 'Device', 'Time_Spent_on_Site', 'Number_of_Pages_Viewed'])
     
+    # create categorical bins for the Income column
     bins = [20000, 40000, 60000, 80000, 100000]
     labels = ['20k-40k', '40k-60k', '60k-80k', '80k-100k']
     df['Income_Range'] = pd.cut(df['Income'], bins=bins, labels=labels, include_lowest=True)
-
+    
+    # create categorical bins for the Age column
     bins = [16, 24, 34, 44, 54, 90]
     labels = ['16-24', '25-34', '35-44', '45-54', '55+']
     df['Age_Range'] = pd.cut(df['Age'], bins=bins, labels=labels, include_lowest=True)
-
+    
     return df
 
 def click_by_category(df):
+    """
+    This function takes a pandas DataFrame (`df`) and creates a bar plot showing the percentage of clicks
+    in each interest category.
+    """
+
+    # pivot the DataFrame to get the total counts for each category and click status
     df_pivot = df.pivot_table(index='Interest_Category', columns='Click', aggfunc='size', fill_value=0)
-
+    
+    # calculate the percentage of clicks for each category
     df_pivot_percentage = df_pivot.div(df_pivot.sum(axis=1), axis=0) * 100
-
+    
+    # plot the bar chart
     ax = df_pivot_percentage.plot(kind='bar', figsize=(8, 6), color=['#d9e6f2', '#4a90e2'])
-
+    
+    # set the y-axis limits
     ax.set_ylim(46, 53)
-
+    
+    # set the x-axis label
     plt.xlabel('Category')
+    
+    # set the y-axis label
     plt.ylabel('Percentage (%)')
+    
+    # set the x-tick rotation
     plt.xticks(rotation=0)
-
+    
+    # add a legend
     plt.legend(title='Click', labels=['No Click', 'Click'])
-
+    
+    # adjust the layout
     plt.tight_layout()
-
+    
+    # show the plot
     plt.show()
 
 def click_by_category_income(df):
-    df_grouped = df.groupby(['Income_Range', 'Interest_Category', 'Click'], observed=False).size().unstack(fill_value=0)
-    df_grouped['Total'] = df_grouped[0] + df_grouped[1]
-    df_grouped['Percentage_Click'] = df_grouped[1] / df_grouped['Total'] * 100
+    """
+    This function takes a pandas DataFrame (`df`) and creates a bar plot showing the percentage of clicks
+    in each interest category for each income range.
+    """
 
+    # group the DataFrame by income range, interest category, and click status
+    df_grouped = df.groupby(['Income_Range', 'Interest_Category', 'Click'], observed=False).size().unstack(fill_value=0)
+    
+    # calculate the total counts for each income range and interest category
+    df_grouped['Total'] = df_grouped[0] + df_grouped[1]
+    
+    # calculate the percentage of clicks for each income range and interest category
+    df_grouped['Percentage_Click'] = df_grouped[1] / df_grouped['Total'] * 100
+    
+    # pivot the DataFrame to create a new DataFrame with the percentage of clicks
     df_pivot = df_grouped.reset_index().pivot(index='Income_Range', columns='Interest_Category', values='Percentage_Click')
 
+    # create the figure and axis
     plt.figure(figsize=(10, 6))
 
+    # plot the bar chart
     ax = df_pivot.plot(kind='bar', stacked=False, colormap='tab10', width=0.8, ax=plt.gca())
+    
+    # set the y-axis limits
     ax.set_ylim(39, 61)
-
+    
+    # set the x-axis label
     plt.xlabel('Income range')
+    
+    # set the y-axis label
     plt.ylabel('Click (%)')
-
+    
+    # add a legend
     plt.legend(title='Category')
+    
+    # rotate the x-axis tick labels
     plt.xticks(rotation=40)
+    
+    # adjust the layout
     plt.tight_layout()
-
+    
+    # show the plot
     plt.show()
 
 def click_by_category_age(df):
+    """
+    This function takes a pandas DataFrame (`df`) and creates a bar plot showing the percentage of clicks
+    in each interest category for each age range.
+    """
+
+    # group the DataFrame by age range and interest category, and calculate the total clicks and total counts
     df_grouped = df.groupby(['Age_Range', 'Interest_Category'], observed=False).agg({'Click': ['sum', 'count']})
+
+    # rename the columns
     df_grouped.columns = ['Total_Clicks', 'Total_Count']
+    
+    # calculate the percentage of clicks for each age range and interest category
     df_grouped['Percentage_Click'] = df_grouped['Total_Clicks'] / df_grouped['Total_Count'] * 100
 
+    # pivot the DataFrame to create a new DataFrame with the percentage of clicks
     df_pivot = df_grouped.reset_index()
     df_pivot =df_pivot.pivot(index='Age_Range', columns='Interest_Category', values='Percentage_Click')
 
+    # create the figure and axis
     plt.figure(figsize=(10, 6))
 
+    # plot the bar chart
     ax = df_pivot.plot(kind='bar', stacked=False, colormap='tab10', width=0.8, ax=plt.gca())
+    
+    # set the y-axis limits
     ax.set_ylim([40, 60])
-
+    
+    # set the x-axis label
     plt.xlabel('Age range')
+    
+    # set the y-axis label
     plt.ylabel('Click (%)')
-
+    
+    # add a legend
     plt.legend(title='Category', loc=('upper left'), bbox_to_anchor=(-0.02, 1))
+    
+    # rotate the x-axis tick labels
     plt.xticks(rotation=45)
+    
+    # adjust the layout
     plt.tight_layout()
-
+    
+    # show the plot
     plt.show()
